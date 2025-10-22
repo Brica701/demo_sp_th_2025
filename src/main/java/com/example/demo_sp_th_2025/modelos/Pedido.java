f35077fc18ee186824a400c787c7315888b08b82
Brica701 <isaacruz2003@gmail.com>
2025-10-22 12:26:35 +0200

    models_realizados

---
+++ src/main/java/com/example/demo_sp_th_2025/modelos/Pedido.java
@@ -0,0 +1,44 @@
+package com.example.demo_sp_th_2025.modelos;
+
+import java.sql.Date;
+
+public class Pedido {
+    private int id;
+    private double total;
+    private Date fecha;
+    private int idCliente;
+    private int idComercial;
+
+    public Pedido() {}
+
+    public Pedido(double total, Date fecha, int idCliente, int idComercial) {
+        this.total = total;
+        this.fecha = fecha;
+        this.idCliente = idCliente;
+        this.idComercial = idComercial;
+    }
+
+    public Pedido(int id, double total, Date fecha, int idCliente, int idComercial) {
+        this.id = id;
+        this.total = total;
+        this.fecha = fecha;
+        this.idCliente = idCliente;
+        this.idComercial = idComercial;
+    }
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public double getTotal() { return total; }
+    public void setTotal(double total) { this.total = total; }
+
+    public Date getFecha() { return fecha; }
+    public void setFecha(Date fecha) { this.fecha = fecha; }
+
+    public int getIdCliente() { return idCliente; }
+    public void setIdCliente(int idCliente) { this.idCliente = idCliente; }
+
+    public int getIdComercial() { return idComercial; }
+    public void setIdComercial(int idComercial) { this.idComercial = idComercial; }
+}
+
