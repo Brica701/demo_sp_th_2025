2496a5ebefe0ceb6633e4847bdff4d3817186eb0
Brica701 <isaacruz2003@gmail.com>
2025-10-22 12:28:55 +0200

    models_realizados_cliente_corregido

--- src/main/java/com/example/demo_sp_th_2025/modelos/Cliente.java
+++ src/main/java/com/example/demo_sp_th_2025/modelos/Cliente.java
@@ -6,45 +6,12 @@ public class Cliente {
     private String apellido1;
     private String apellido2;
     private String ciudad;
-    private Integer categoria; // Puede ser null, por eso Integer y no int
+    private int categoria; // Puede ser null, por eso Integer y no int
 
-    // Constructor vacío
-    public Cliente() {}
 
-    public Cliente(String nombre, String apellido1, String apellido2, String ciudad, Integer categoria) {
-        this.nombre = nombre;
-        this.apellido1 = apellido1;
-        this.apellido2 = apellido2;
-        this.ciudad = ciudad;
-        this.categoria = categoria;
-    }
 
-    public Cliente(int id, String nombre, String apellido1, String apellido2, String ciudad, Integer categoria) {
-        this.id = id;
-        this.nombre = nombre;
-        this.apellido1 = apellido1;
-        this.apellido2 = apellido2;
-        this.ciudad = ciudad;
-        this.categoria = categoria;
-    }
 
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
 
-    public String getNombre() { return nombre; }
-    public void setNombre(String nombre) { this.nombre = nombre; }
-
-    public String getApellido1() { return apellido1; }
-    public void setApellido1(String apellido1) { this.apellido1 = apellido1; }
-
-    public String getApellido2() { return apellido2; }
-    public void setApellido2(String apellido2) { this.apellido2 = apellido2; }
-
-    public String getCiudad() { return ciudad; }
-    public void setCiudad(String ciudad) { this.ciudad = ciudad; }
-
-    public Integer getCategoria() { return categoria; }
-    public void setCategoria(Integer categoria) { this.categoria = categoria; }
 }
 
 
