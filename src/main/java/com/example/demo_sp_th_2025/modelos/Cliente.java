926663d34bdc67b1110cc744bec875d9b1a2b0fd
Brica701 <isaacruz2003@gmail.com>
2025-10-23 10:45:48 +0200

    models_realizados_todos_corregido_final

--- src/main/java/com/example/demo_sp_th_2025/modelos/Cliente.java
+++ src/main/java/com/example/demo_sp_th_2025/modelos/Cliente.java
@@ -1,5 +1,8 @@
 package com.example.demo_sp_th_2025.modelos;
 
+import lombok.Data;
+
+@Data
 public class Cliente {
     private int id;
     private String nombre;
@@ -8,87 +11,6 @@ public class Cliente {
     private String ciudad;
     private int categoria;
 
-    public Cliente(int id, String nombre, String apellido1, String apellido2, String ciudad, int categoria) {
-        this.id = id;
-        this.nombre = nombre;
-        this.apellido1 = apellido1;
-        this.apellido2 = apellido2;
-        this.ciudad = ciudad;
-        this.categoria = categoria;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getApellido1() {
-        return apellido1;
-    }
-
-    public void setApellido1(String apellido1) {
-        this.apellido1 = apellido1;
-    }
-
-    public String getApellido2() {
-        return apellido2;
-    }
-
-    public void setApellido2(String apellido2) {
-        this.apellido2 = apellido2;
-    }
-
-    public String getCiudad() {
-        return ciudad;
-    }
-
-    public void setCiudad(String ciudad) {
-        this.ciudad = ciudad;
-    }
-
-    public int getCategoria() {
-        return categoria;
-    }
-
-    public void setCategoria(int categoria) {
-        this.categoria = categoria;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Cliente cliente = (Cliente) o;
-        return id == cliente.id;
-    }
-
-    @Override
-    public int hashCode() {
-        return id;
-    }
-
-    @Override
-    public String toString() {
-        return "Cliente{" +
-                "id=" + id +
-                ", nombre='" + nombre + '\'' +
-                ", apellido1='" + apellido1 + '\'' +
-                ", apellido2='" + apellido2 + '\'' +
-                ", ciudad='" + ciudad + '\'' +
-                ", categoria=" + categoria +
-                '}';
-    }
 }
 
 
