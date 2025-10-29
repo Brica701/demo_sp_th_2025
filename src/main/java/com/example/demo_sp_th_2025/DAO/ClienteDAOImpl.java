4843b135ee2c81bf7161de1b1a5a0bbd8dcd537f
Brica701 <isaacruz2003@gmail.com>
2025-10-29 12:23:00 +0100

    ClienteDAOImpl_terminado

--- src/main/java/com/example/demo_sp_th_2025/DAO/ClienteDAOImpl.java
+++ src/main/java/com/example/demo_sp_th_2025/DAO/ClienteDAOImpl.java
@@ -6,9 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.sql.init.SqlInitializationProperties;
 import org.springframework.context.annotation.AdviceModeImportSelector;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.servlet.tags.form.SelectTag;
 
+import java.sql.PreparedStatement;
 import java.util.List;
 import java.util.Optional;
 
@@ -28,7 +31,38 @@ public class ClienteDAOImpl implements ClienteDAO {
     @Override
     public void create(Cliente cliente) {
 
+        //Desde java15+ se tiene la triple quote """ para bloques de texto como cadenas.
+        String sqlInsert = """
+							INSERT INTO cliente (nombre, apellido1, apellido2, ciudad, categoría) 
+							VALUES  (     ?,         ?,         ?,       ?,         ?)
+						   """;
 
+
+        //Sin recuperación de id generado
+//		int rows = jdbcTemplate.update(sqlInsert,
+//							cliente.getNombre(),
+//							cliente.getApellido1(),
+//							cliente.getApellido2(),
+//							cliente.getCiudad(),
+//							cliente.getCategoria()
+//					);
+
+        //Con recuperación de id generado
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        int rows = jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(sqlInsert, new String[] { "id" });
+            int idx = 1;
+            ps.setString(idx++, cliente.getNombre());
+            ps.setString(idx++, cliente.getApellido1());
+            ps.setString(idx++, cliente.getApellido2());
+            ps.setString(idx++, cliente.getCiudad());
+            ps.setInt(idx, cliente.getCategoria());
+            return ps;
+        },keyHolder);
+
+        cliente.setId(keyHolder.getKey().intValue());
+
+        log.info("Insertados {} registros.", rows);
     }
 
     @Override
@@ -54,16 +88,55 @@ public class ClienteDAOImpl implements ClienteDAO {
 
     @Override
     public Optional<Cliente> find(int id) {
-        return Optional.empty();
+
+        Cliente fab =  jdbcTemplate
+                .queryForObject("SELECT * FROM cliente WHERE id = ?"
+                        , (rs, rowNum) -> new Cliente(rs.getInt("id"),
+                                rs.getString("nombre"),
+                                rs.getString("apellido1"),
+                                rs.getString("apellido2"),
+                                rs.getString("ciudad"),
+                                rs.getInt("categoría"))
+                        , id
+                );
+
+        if (fab != null) {
+            return Optional.of(fab);}
+        else {
+            log.info("Cliente no encontrado.");
+            return Optional.empty(); }
+
     }
 
     @Override
     public void update(Cliente cliente) {
+        int rows = jdbcTemplate.update("""
+										UPDATE cliente SET 
+														nombre = ?, 
+														apellido1 = ?, 
+														apellido2 = ?,
+														ciudad = ?,
+														categoría = ?  
+												WHERE id = ?
+										""", cliente.getNombre()
+                , cliente.getApellido1()
+                , cliente.getApellido2()
+                , cliente.getCiudad()
+                , cliente.getCategoria()
+                , cliente.getId());
+
+        log.info("Update de Cliente con {} registros actualizados.", rows);
 
     }
+
 
     @Override
     public void delete(int id) {
 
+        int rows = jdbcTemplate.update("DELETE FROM cliente WHERE id = ?", id);
+
+        log.info("Delete de Cliente con {} registros eliminados.", rows);
+
     }
+
 }
